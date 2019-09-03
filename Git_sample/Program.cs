827f6f61340d612f1f158fbf7a982483602baeeb
悠生 小島 <kojima.yuusei@medopty.com>
2019-09-03 15:35:38 +0900

    aaaaaa

--- Git_sample/Program.cs
+++ Git_sample/Program.cs
@@ -2,12 +2,16 @@
 
 namespace Git_sample
 {
+
+    
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("!!!!!!11");
+            Console.WriteLine("!!!!!!!!");
+            Console.WriteLine("aaaaaaa");
+
         }
     }
 }
